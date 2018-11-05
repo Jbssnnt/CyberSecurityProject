0a1134da8f473fbfb592c5ec5fcfbc4d6b349e31
LiamPilling <lami.pilling@gmail.com>
2018-11-05 14:29:25 +0800

    Added the shared object stuff including grabbing .so's linked before and during the trace

--- tracer.py
+++ tracer.py
@@ -1,10 +1,31 @@
 import os
 import sys
 import time
+import re
 
-def writeSummary():
+objectRegex = re.compile('''
+(.*\.so.*)
+''', re.VERBOSE)
+
+def getSOMaps(pidlist):
+    maplist = []
+    for pid in pidlist:
+        mapfile = open('/proc/' + pid + '/maps', 'r')
+        maps = mapfile.readlines()
+        for line in maps:
+            line = line.strip()
+            soline = objectRegex.search(line)
+            if soline:
+                mapline = ('[' + '{0:5d}'.format(int(pid)) + '] ' + soline.group().split()[5])
+                if mapline not in maplist:
+                    maplist.append(mapline)
+    return maplist
+
+def writeSummary(processes):
     callType = []
     occur = []
+    name = "N/A"
+    totalCalls = 0
     outfile = open('TraceCallSummary.txt', 'w')
     p = os.popen('ls -v /tmp/tracer/', 'r')
     lines = p.readlines()
@@ -20,13 +41,18 @@ def writeSummary():
                 else:
                     occur[callType.index(calls[1].split('(')[0])] += 1
             outfile.write('--------------------------------------------------\n')
-            outfile.write('Calls by PID ' + line[17:-1] + '\n')
+            for proc in processes:
+                proc = proc.split()
+                if line[17:-1] == proc[1]:
+                    name = proc[7]
+            outfile.write('Calls by PID ' + line[17:-1] + ' | ' + name + '\n')
             outfile.write('Calls   | CallType\n')
             for i in range(0, len(occur)):
                 outfile.write('{0:7d} | '.format(occur[i]))
                 outfile.write(callType[i] + '\n')
         callType = []
         occur = []
+        name = "N/A"
 
 def writeRawData():
     calllist = []
@@ -43,12 +69,63 @@ def writeRawData():
     for line in calllist:
         outfile.write(line)
 
+def writeMaps(maplist, processes):
+    runtime = []
+    fileobj = open('TraceCallRaw.txt', 'r')
+    calls = fileobj.readlines()
+    for call in calls:
+        call = call.strip()
+        soline = objectRegex.search(call)
+        if soline:
+            soline = soline.group().split('"')
+            for word in soline:
+                object = objectRegex.search(word)
+                if object:
+                    newObj = "[" + call[1:6] + "] " + object.group()
+                    if newObj not in runtime:
+                        runtime.append(newObj)
+
+    outfile = open('MapSummary.txt', 'w')
+    name = 'N/A'
+    currpid = 1
+    outfile.write('---------SHARED OBJECTS LINKED DURING RUNTIME---------')
+    for mapline in runtime:
+        pid = str(int(mapline[1:6]))
+        for proc in processes[1:]:
+            proc = proc.split()
+            if pid == proc[1]:
+                name = proc[7]
+        if pid != currpid:
+            outfile.write('--------------------------------------------------\n')
+            outfile.write('PID: ' + '{0:5d}'.format(int(pid)) + ' | Process Path: ' + name + '\n')
+            outfile.write('\t' + mapline[8:] + '\n')
+            currpid = pid
+        else:
+            outfile.write('\t' + mapline[8:] + '\n')
+        name = 'N/A'
+
+    outfile.write('\n\n\n---------SHARED OBJECTS LINKED BEFORE RUNTIME---------')
+    for mapline in maplist:
+        pid = str(int(mapline[1:6]))
+        for proc in processes[1:]:
+            proc = proc.split()
+            if pid == proc[1]:
+                name = proc[7]
+        if pid != currpid:
+            outfile.write('--------------------------------------------------\n')
+            outfile.write('PID: ' + '{0:5d}'.format(int(pid)) + ' | Process Path: ' + name + '\n')
+            outfile.write('\t' + mapline[8:] + '\n')
+            currpid = pid
+        else:
+            outfile.write('\t' + mapline[8:] + '\n')
+        name = 'N/A'
+
 def main():
     if(os.getuid() != 0):
         print("Unable to run with lower privileges not recommended, refer to manual")
         exit()
 
-    checkstrace = os.popen('ls /usr/bin/ | grep strace', 'r')
+    checkstrace = os.popen('ls /usr/bin | grep strace', 'r')
     if len(checkstrace.readlines()) == 0:
         print("Program requires strace for operation\nTry installing using:")
         print("\n\tsudo apt-get install strace\n")
@@ -59,23 +136,27 @@ def main():
 
     os.popen('mkdir /tmp/tracer')
     proc = os.popen('ps -ef', 'r')
-    lines = proc.readlines()
-    for line in lines[1:-2]:
+    processes = proc.readlines()
+    for line in processes[1:-2]:
         words = line.split()
         if words[7][0] != '[':
             pidlist.append(words[1])
 
+    maplist = getSOMaps(pidlist)
     # execute the strace program for each PID, catching all processes
     cmd = "timeout " + sys.argv[1] + " strace -q -ff -ttt -o /tmp/tracer/TraceCallRaw.txt"
+    print("Beginning tracing of processes")
     for pid in pidlist:
         cmd = cmd + " -p " + str(pid)
     cmd = cmd + " &"
     out = os.popen(cmd)
 
     time.sleep(int(sys.argv[1]))
-
-    writeSummary()
+    print("Tracing complete, generating summary")
+    writeSummary(processes)
     writeRawData()
+    writeMaps(maplist, processes)
+    print("Program complete")
     os.popen('rm -rf /tmp/tracer')
 
 if __name__ == '__main__':
